a6e368f3a306b51420ced51d1cbcb6fb5e976517
unknown <you@example.com>
2020-06-10 15:00:19 +0100

    first commit

---
+++ smartflow/src/main/java/com/gateway/smartflow/SmartflowApp.java
@@ -0,0 +1,13 @@
+package com.gateway.smartflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartflowApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartflowApp.class, args);
+	}
+
+}
